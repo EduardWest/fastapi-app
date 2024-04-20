96e3892fd2c34ea455e0941d18483417bdd965eb
EduardWest <60728891+EduardWest@users.noreply.github.com>
2024-04-20 18:14:03 +0300

    Update main.py
    First commit

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-from typing import Optional
-
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/task/", response_class=HTMLResponse)
+async def read_item(name, message):
+    return f"""
+    <html>
+        <head>
+            <title>Recruto task</title>
+        </head>
+        <body>
+            <h2>Hello, {name}! {message} </h2>
+        </body>
+    </html>"""
